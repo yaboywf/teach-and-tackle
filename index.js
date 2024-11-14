e451f3e1ebdd191b0d0391c81f73c7e1753444c1
yaboywf <149309816+yaboywf@users.noreply.github.com>
2024-11-14 18:48:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-alert("This is a simplified version of the original platform, with key features removed to prevent plagiarism.");
-alert("© yaboywf 2024. All rights reserved.")
+alert("This is a simplified version of the original platform, with key features removed to prevent plagiarism. © yaboywf 2024. All rights reserved.");
